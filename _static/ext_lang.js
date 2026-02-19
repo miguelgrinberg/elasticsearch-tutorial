72a2a01c93b12756b2e261dadc060ad5bb44821c
Miguel Grinberg <miguel.grinberg@gmail.com>
2026-02-19 17:01:16 +0000

    myst-parser

---
+++ _static/ext_lang.js
@@ -0,0 +1,76 @@
+function getCurrentLang() {
+  const lang = window.sessionStorage.getItem(storageKeyPrefix + 'lang');
+  if (!lang) {
+    const firstOption = document.querySelector('#lang-select option');
+    return firstOption.value;
+  }
+  return lang;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // insert language selector
+  const langSelect = document.createElement('select');
+  langSelect.setAttribute('id', 'lang-select');
+  langSelect.innerHTML = `
+    <option value="py">Python</option>
+    <option value="js">JavaScript</option>
+  `;
+  const iconContainer = document.querySelector('.content-icon-container');
+  iconContainer.insertBefore(langSelect, iconContainer.firstChild);
+
+  // language selector
+  const lang = getCurrentLang();
+  //const langSelect = document.getElementById('lang-select');
+  langSelect.value = lang;
+  langSelect.addEventListener('change', () => {
+    const langSelect = document.getElementById('lang-select');
+    const tab = document.querySelector(`[data-sync-group="lang"][data-sync-id="${langSelect.value}"]`);
+    if (tab) {
+      tab.click();
+    }
+  });
+
+  // hook into tabbed components to update language identifiers
+  document.querySelectorAll(".sd-tab-label").forEach((label) => {
+    label.addEventListener('click', updateLang);
+  });
+
+  // update language identifiers
+  updateLang();
+});
+
+function updateLang() {
+  const lang = getCurrentLang();
+  const langSelect = document.getElementById('lang-select');
+  langSelect.value = lang;
+  document.querySelectorAll('.lang-choices').forEach(elem => {
+    if (!elem.langChoices) {
+      const choices = {}
+      elem.innerHTML.split(',').forEach(s => {
+        if (!s.includes(':')) {
+          choices[''] = s;
+        }
+        else {
+          const [l, ...ids] = s.split(':');
+          const langs = l.split('|');
+          langs.forEach(lang => choices[lang] = ids.join(':'));
+        }
+      });
+      elem.langChoices = choices;
+    }
+
+    let choice;
+    if (elem.langChoices[lang]) {
+      choice = elem.langChoices[lang];
+    }
+    else {
+      choice = elem.langChoices[''] || '';
+    }
+    if (elem.classList.contains('lang-id')) {
+      elem.innerHTML = '<code class="docutils literal notranslate"><span class="pre">' + choice + '</code>';
+    }
+    else {
+      elem.innerHTML = choice;
+    }
+  });
+}
--- conf.py
+++ conf.py
@@ -3,22 +3,34 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path('_ext').resolve()))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'elasticsearch-tutorial'
 copyright = '2026, Miguel Grinberg'
 author = 'Miguel Grinberg'
+version = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinx_design', 'sphinx_copybutton', 'sphinx_prompt', 'sphinxcontrib.mermaid']
+extensions = [
+    'myst_parser',
+    'sphinx_design',
+    'sphinx_copybutton',
+    'sphinxcontrib.mermaid',
+    'lang',
+]
 myst_enable_extensions = ['colon_fence', 'smartquotes', 'replacements', 'linkify', 'deflist']
 myst_heading_anchors = 3
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
 
 
 
@@ -26,4 +38,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
+html_title = 'Elasticsearch Tutorial'
 html_static_path = ['_static']
+html_js_files = ['ext_lang.js']
+html_css_files = ['ext_lang.css']
